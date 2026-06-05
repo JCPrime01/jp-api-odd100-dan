cfed57fb74cbab6423ef7978120b474e357eb7b5
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 17:53:31 -0300

    chore: shareCode ROI20RQZBHH -> NSQACEYBC0T (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=ROI20RQZBHH&afp=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=NSQACEYBC0T&afp=tp' });
     res.end();
   }
